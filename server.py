cac40d0eab022d93c184f585d3bf3d1c5a4e0927
michael.fu <michael.fu@taiwanscale.com>
2026-07-21 01:00:57 +0800

    feat: disable browser caching for static assets by sending cache control headers

--- server.py
+++ server.py
@@ -18,6 +18,12 @@ class PLMRequestHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
     def get_db(self):
         conn = sqlite3.connect(DB_PATH)
         conn.row_factory = sqlite3.Row
